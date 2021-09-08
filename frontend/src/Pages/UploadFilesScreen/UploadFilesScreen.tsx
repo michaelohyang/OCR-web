fbd8e7710bb1f130a46fb7501c76c462d9cd824d
Michael Oh-Yang <ohyangmichael123@gmail.com>
2021-09-08 12:54:56 -0400

    added upload screen

--- frontend/src/Pages/UploadFilesScreen/UploadFilesScreen.tsx
+++ frontend/src/Pages/UploadFilesScreen/UploadFilesScreen.tsx
@@ -1,8 +1,24 @@
+import { Grid } from "@chakra-ui/layout";
 import { Component } from "react";
+import NavBar from "./Components/NavBar";
+
+var styles = {
+  container: {
+    background: "#00A36C",
+    height: "50em",
+  },
+};
 
 class UploadFilesScreen extends Component {
   render() {
-    return <p> Hello World </p>;
+    return (
+      <div>
+        <div>
+          <NavBar />
+        </div>
+        <div style={styles.container}></div>
+      </div>
+    );
   }
 }
 
