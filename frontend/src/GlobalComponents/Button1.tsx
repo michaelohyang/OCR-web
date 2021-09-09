51507cca2c0a6f437ba0ff7c7c9e42a378d9f64f
trevoryo <72239283+trevoryo@users.noreply.github.com>
2021-09-09 18:03:36 -0400

    updated1

--- frontend/src/GlobalComponents/Button1.tsx
+++ frontend/src/GlobalComponents/Button1.tsx
@@ -3,11 +3,12 @@ import '../App.css';
 import * as React from "react";
 import {Button} from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
+
+// This is the function for all clickable buttons which can change the name and achieve external function 
 export interface name_to_change {
   txtname:string;
   onClickFunc: () => void;
 }
-
 
 export default function Buttonfuc( { txtname, onClickFunc } : name_to_change ){
     return <Button 
