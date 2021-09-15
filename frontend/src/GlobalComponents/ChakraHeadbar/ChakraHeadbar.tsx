54d8cf6f53ad525a60d7032a49f2fc750acfdf29
Michael Oh-Yang <mohyang3@gatech.edu>
2021-09-15 15:58:51 -0400

    update headbar

--- frontend/src/GlobalComponents/ChakraHeadbar/ChakraHeadbar.tsx
+++ frontend/src/GlobalComponents/ChakraHeadbar/ChakraHeadbar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Logofunc from "./logo";
-import "../App.css";
-import "./logo.css";
+import "./ChakraHeadbar.css";
 import { HStack, Image, Text, Center } from "@chakra-ui/react";
 import logo1 from "./logo.jpg";
 import Menufunc from "./Menu1";
@@ -12,12 +11,9 @@ export default class ChakraHeadbar extends React.Component {
       <div>
         <HStack className="container">
           <Logofunc />
-          <Center>
-            <Image src={logo1} className="header" />
-            <Text className="centerword" align="center">
-              Online Medical Record Scanner
-            </Text>
-          </Center>
+          <div className="text">
+            <Text>Online Medical Record Scanner</Text>
+          </div>
           <div className="menu">
             <Menufunc />
           </div>
