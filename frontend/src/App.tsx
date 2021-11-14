bf295e3211247f240a3dfdc467960b4c0160794d
michaelohyang <43708401+michaelohyang@users.noreply.github.com>
2021-11-14 13:16:02 -0500

    Develop/integration (#39)
    * Merged CreateNewProjectPage into ProjectMain
    * Merged ProjectMain into develop/integration
    * updated package-lock
    Co-authored-by: PeggyZhao2 <peggy-zhao@outlook.com>
    Co-authored-by: Josh Chen <60103135+spicyTofuu@users.noreply.github.com>
    Co-authored-by: Shuge Fan <52257702+fan2088@users.noreply.github.com>
    Co-authored-by: trevoryo <72239283+trevoryo@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UploadFilesScreen from "./Pages/UploadFilesScreen/UploadFilesScreen";
 import ConfirmFilesScreen from "./Pages/ConfirmDigitalForm/ConfirmDigitalForm";
+import ProjectMain from "./Pages/ProjectMain/ProjectMain";
+import CreateProject from "./Pages/CreateNewProjectPage/CreateNewProjectPage";
 
 class App extends Component {
   render() {
@@ -11,10 +13,19 @@ class App extends Component {
         <div>
           <Switch>
             <Route path="/" exact>
+              <ProjectMain />
+            </Route>
+            <Route path="/createProject">
+              <CreateProject />
+            </Route>
+            <Route path="/upload">
               <UploadFilesScreen />
             </Route>
             <Route path="/confirm">
               <ConfirmFilesScreen />
+            </Route>
+            <Route>
+              <UploadFilesScreen />
             </Route>
           </Switch>
         </div>
