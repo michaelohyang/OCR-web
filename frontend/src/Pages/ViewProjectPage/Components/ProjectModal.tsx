2accfa59191b5c98266a299b1e29ec67999f2e72
PeggyZhao2 <peggy-zhao@outlook.com>
2021-11-27 14:05:21 -0500

    new pages

--- frontend/src/Pages/ViewProjectPage/Components/ProjectModal.tsx
+++ frontend/src/Pages/ViewProjectPage/Components/ProjectModal.tsx
@@ -12,6 +12,7 @@ interface ProjectModalInterface {
   projects: any;
   k: string;
   removeProject: Function;
+  selectProject: Function;
 }
 
 function ProjectModal(props: ProjectModalInterface) {
@@ -28,7 +29,7 @@ function ProjectModal(props: ProjectModalInterface) {
         marginTop="0.5em"
         className="modalBox"
       >
-        <Link to="/upload">
+        <Link to="/existDigitalForm" onClick={() => props.selectProject(props.k)}>
           <img
             src={colors[Math.floor(Math.random() * colors.length)]}
             alt="imagePreview"
