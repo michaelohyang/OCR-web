9160a6a9a06d2441bdb7dcda1041b50e9c323a47
trevoryo <72239283+trevoryo@users.noreply.github.com>
2021-12-03 01:07:17 -0500

    content layout

--- frontend/src/Pages/ExistDigitalForm/ExistDigitalForm.tsx
+++ frontend/src/Pages/ExistDigitalForm/ExistDigitalForm.tsx
@@ -63,9 +63,9 @@ class ExistDigitalForm extends Component<any, any> {
         console.log(key + ": " + rawJson[patient][key]);
         const k = key;
         eachform.push(
-          <div key={countOfRows}>
-            <HStack >
-              <div>{k}</div>
+          <div key={countOfRows} className="existinfo">
+            <HStack overflowX="auto">
+              <div className="textdiv1">{k}:</div>
               <div>{rawJson[p][k]}</div>
             </HStack>
           </div>
@@ -73,7 +73,7 @@ class ExistDigitalForm extends Component<any, any> {
         countOfRows++;
       }
       this.state.forms.push(
-        <div key={1000 + p} className="existinfo">
+        <div key={1000 + p}>
           {eachform}
         </div>
       );
